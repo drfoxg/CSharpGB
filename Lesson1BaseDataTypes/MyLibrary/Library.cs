ed6496393ca846a258bd3547c30b6803024c14df
Anatoly Borisov <drfoxg@gmail.com>
2021-06-16 22:24:21 +0300

    All job in Lesson 1

--- Lesson1BaseDataTypes/MyLibrary/Library.cs
+++ Lesson1BaseDataTypes/MyLibrary/Library.cs
@@ -6,7 +6,13 @@ using System.Text;
 namespace MyLibrary
 {
     // моя библотека
-	
+
+    /*public enum ConsolePos
+    {
+        TopLeft, TopRight, Centre, BottomLeft, BottomRitgh
+    }
+    */
+
     public class Library
     {
         public static void Pause()
@@ -42,6 +48,47 @@ namespace MyLibrary
 
             Console.BackgroundColor = def_backgroundcolor;
             Console.ForegroundColor = def_foregroundcolor;
+        }
+
+        /*public static void Print(ConsolePos pos, string message)
+        { 
+            
+        }*/
+
+        public static void PrintCentre(string message)
+        {
+            int centerX = (Console.WindowWidth / 2) - (message.Length / 2);
+            int centerY = (Console.WindowHeight / 2) - 1;
+            Console.SetCursorPosition(centerX, centerY);
+            Console.Write(message);
+        }
+
+        public static void PrintTopLeft(string message)
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.Write(message);
+        }
+
+        public static void PrintTopRight(string message)
+        {
+            int topRightX = Console.WindowWidth - message.Length;
+            Console.SetCursorPosition(topRightX, 0);
+            Console.Write(message);
+        }
+
+        public static void PrintBottomLeft(string message)
+        {
+            int bottomY = Console.WindowHeight - 1;
+            Console.SetCursorPosition(0, bottomY);
+            Console.Write(message);
+        }
+
+        public static void PrintBottomRight(string message)
+        {
+            int bottomRightX = Console.WindowWidth - message.Length;
+            int bottomY = Console.WindowHeight - 1;
+            Console.SetCursorPosition(bottomRightX, bottomY);
+            Console.Write(message);
         }
     }
 }
---
+++ Lesson1BaseDataTypes/ShowName/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+using MyLibrary;
+
+namespace ShowName
+{
+    class Program
+    {
+        /*
+         * 5. а) Написать программу, которая выводит на экран ваше имя, фамилию и город проживания.
+         * б) *Сделать задание, только вывод организовать в центре экрана.
+         * в) **Сделать задание б с использованием собственных методов(например, Print(string ms, int x, int y).
+         * 
+         */
+
+        static void Main(string[] args)
+        {
+            Library.Print("Ваше имя:");
+            string name = ReadLine();
+
+            Library.Print("Ваша фамилия:");
+            string surname = ReadLine();
+
+            Library.Print("Ваш город:");
+            string town = ReadLine();
+
+            Console.Clear();
+
+            Library.PrintCentre(name + " " + surname + " " + town);
+            Library.PrintBottomLeft("Нажмите любую кнопку для завершения программы");
+
+            Library.Pause();
+
+        }
+    }
+}
