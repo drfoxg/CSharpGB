44cb13eff479f26d21b1d33dbc2215765a7ec7e9
Anatoly Borisov <drfoxg@gmail.com>
2021-06-16 21:21:56 +0300

    New job in Lesson 1

--- Lesson1BaseDataTypes/MyLibrary/Library.cs
+++ Lesson1BaseDataTypes/MyLibrary/Library.cs
@@ -13,5 +13,35 @@ namespace MyLibrary
         {
             Console.ReadKey();
         }
+        public static void Pause(string msg)
+        {
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+
+        public static void Print(string message)
+        {
+            ConsoleColor def_backgroundcolor = Console.BackgroundColor;
+            ConsoleColor def_foregroundcolor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+
+            Console.BackgroundColor = def_backgroundcolor;
+            Console.ForegroundColor = def_foregroundcolor;
+        }
+
+        public static void Print(int x, int y, string message)
+        {
+            ConsoleColor def_backgroundcolor = Console.BackgroundColor;
+            ConsoleColor def_foregroundcolor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(message);
+
+            Console.BackgroundColor = def_backgroundcolor;
+            Console.ForegroundColor = def_foregroundcolor;
+        }
     }
 }
